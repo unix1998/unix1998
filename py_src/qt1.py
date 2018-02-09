475cd5069dd72227c4ba67aa53175a2c34a79254
Unix1998 <test_98@yahoo.com>
2018-02-09 15:49:00 -0500

    new code

---
+++ py_src/qt1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+import sys
+from PyQt4 import QtGui
+
+app = QtGui.QApplication(sys.argv)
+
+window = QtGui.QWidget()
+window.setGeometry(0, 0, 500, 300)
+window.setWindowTitle("PyQT Tuts!")
+
+window.show()
